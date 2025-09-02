2d1fe6fd07a3d5db7436a718870b80edb3e43eb6
Luka Lopin <lukabmx69@gmail.com>
2025-09-02 18:39:08 +0200

    add .gitignore file not to track build, modify startproject.bat not to get path error

--- StartProject.bat
+++ StartProject.bat
@@ -3,6 +3,5 @@ REM This batch file opens the project in VS Code
 echo Initializing GRBL submodule...
 git submodule update --init --recursive
 echo Starting VS Code for GRBL Development...
-cd /d "%~dp0"  # Changed from "src" to root directory
 code .
 pause

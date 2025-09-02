263aeac9b75637ed2f2c4b2aeb89acd11db5be88
Luka Lopin <lukabmx69@gmail.com>
2025-09-02 17:59:41 +0200

    add submodule update in StartProject.bat, which pulls grbl repo

--- StartProject.bat
+++ StartProject.bat
@@ -1,5 +1,7 @@
 @echo off
 REM This batch file opens the project in VS Code
+echo Initializing GRBL submodule...
+git submodule update --init --recursive
 echo Starting VS Code for GRBL Development...
 cd /d "%~dp0"  # Changed from "src" to root directory
 code .

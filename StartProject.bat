158236ef9f987c523c87aa590281b5072ea3b879
Luka Lopin <lukabmx69@gmail.com>
2025-09-02 17:55:05 +0200

    updated project structure

--- StartProject.bat
+++ StartProject.bat
@@ -1,6 +1,6 @@
 @echo off
 REM This batch file opens the project in VS Code
 echo Starting VS Code for GRBL Development...
-cd /d "%~dp0src"  
-code ..
+cd /d "%~dp0"  # Changed from "src" to root directory
+code .
 pause

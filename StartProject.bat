5b28f392cf16c1d7b2c6eb72bfbb7c2cfb7354c3
Luka Lopin <lukabmx69@gmail.com>
2025-09-02 16:32:30 +0200

    update json commands and start project paths

--- StartProject.bat
+++ StartProject.bat
@@ -1,6 +1,6 @@
 @echo off
-REM This batch file opens the GRBL project in VS Code
+REM This batch file opens the project in VS Code
 echo Starting VS Code for GRBL Development...
-cd /d "%~dp0grbl"
-code .
+cd /d "%~dp0src"  
+code ..
 pause
